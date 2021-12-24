85f03365346e466246c79f9716346ce67a47132a
Akd <abdelkarimcherif5@gmail.com>
2021-12-24 09:06:13 +0100

    ok

---
+++ defi_3/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+let selectH1Id = document.getElementById("title2");
+console.log(selectH1Id);
+selectH1Id.style.transform = " translate(-120%,-56%)";
+selectH1Id.style.animation = " showTitle 3s infinite";
